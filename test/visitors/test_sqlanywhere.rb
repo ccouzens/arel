4128028210625e31212427fb7b1febe79a33bd0a
Chris Couzens <ccouzens@gmail.com>
2013-02-06 22:56:27 +0000

    Add limit and offset tests
    One currently fails

--- test/visitors/test_sqlanywhere.rb
+++ test/visitors/test_sqlanywhere.rb
@@ -5,6 +5,36 @@ module Arel
     describe "the sqlanywhere visitor" do
       before do
         @visitor = SQLAnywhere.new Table.engine.connection
+        #@table = Arel::Table.new "users"
+      end
+
+      it 'limit is top' do
+        stmt = Nodes::SelectStatement.new
+        stmt.limit = Nodes::Limit.new(10)
+        sql = @visitor.accept(stmt)
+        sql.must_match /TOP/
+      end
+
+      it 'offset is start at' do
+        stmt = Nodes::SelectStatement.new
+        stmt.offset = Nodes::Offset.new(10)
+        sql = @visitor.accept(stmt)
+        sql.must_match /START AT/
+      end
+
+      it 'offset implies limit' do
+        stmt = Nodes::SelectStatement.new
+        stmt.offset = Nodes::Offset.new(10)
+        sql = @visitor.accept(stmt)
+        sql.must_match /TOP/
+      end
+
+      it 'limit goes between SELECT and projection' do
+        stmt = Nodes::SelectStatement.new
+        stmt.limit = Nodes::Limit.new(10)
+        stmt.cores.first.projections << Arel::Nodes::SqlLiteral.new('*') 
+        sql = @visitor.accept(stmt)
+        sql.must_be_like "SELECT TOP 10 *"
       end
     end
   end
